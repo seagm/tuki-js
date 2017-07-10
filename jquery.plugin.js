3a19d8b9f276fd519102c58457d4260f82e7552c
大豆 <ranh@qq.com>
2017-07-11 02:07:45 +0800

    add jquery plugins and php functions

---
+++ jquery.plugin.js
@@ -0,0 +1,11 @@
+(function( $ ) {
+ 
+    $.fn.openPopup = function() {
+        // Open popup code.
+    };
+ 
+    $.fn.closePopup = function() {
+        // Close popup code.
+    };
+ 
+}( jQuery ));
---
+++ php.func.js
@@ -0,0 +1,1 @@
+php.func.js
