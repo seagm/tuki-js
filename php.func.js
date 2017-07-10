a08e9e5884564ae96b68afe8bd508e4530087afd
大豆 <ranh@qq.com>
2017-07-11 02:15:23 +0800

    fix bug

--- php.func.js
+++ php.func.js
@@ -1,1 +1,98 @@
-php.func.js
+//php js
+function str_repeat(input, multiplier) {
+    return new Array(multiplier + 1).join(input);
+}
+
+function number_format(number, decimals, dec_point, thousands_sep) {
+    number = (number + '').replace(/[^0-9+\-Ee.]/g, '');
+    var n = !isFinite(+number) ? 0 : +number,
+        prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
+        sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
+        dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
+        s = '',
+        toFixedFix = function (n, prec) {
+            var k = Math.pow(10, prec);
+            return '' + Math.round(n * k) / k;
+        };
+    // Fix for IE parseFloat(0.55).toFixed(0) = 0;
+    s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
+    if (s[0].length > 3) {
+        s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+    }
+    if ((s[1] || '').length < prec) {
+        s[1] = s[1] || '';
+        s[1] += new Array(prec - s[1].length + 1).join('0');
+    }
+    return s.join(dec);
+}
+
+function nl2br(str) {
+    return str.replace(/\n/g, '<br>');
+}
+
+function nl2br(str, is_xhtml) {
+    //  discuss at: http://phpjs.org/functions/nl2br/
+    // original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // improved by: Philip Peterson
+    // improved by: Onno Marsman
+    // improved by: Atli Þór
+    // improved by: Brett Zamir (http://brett-zamir.me)
+    // improved by: Maximusya
+    // bugfixed by: Onno Marsman
+    // bugfixed by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    //    input by: Brett Zamir (http://brett-zamir.me)
+    //   example 1: nl2br('Kevin\nvan\nZonneveld');
+    //   returns 1: 'Kevin<br />\nvan<br />\nZonneveld'
+    //   example 2: nl2br("\nOne\nTwo\n\nThree\n", false);
+    //   returns 2: '<br>\nOne<br>\nTwo<br>\n<br>\nThree<br>\n'
+    //   example 3: nl2br("\nOne\nTwo\n\nThree\n", true);
+    //   returns 3: '<br />\nOne<br />\nTwo<br />\n<br />\nThree<br />\n'
+
+    var breakTag = (is_xhtml || typeof is_xhtml === 'undefined') ? '<br ' + '/>' : '<br>'; // Adjust comment to avoid issue on phpjs.org display
+
+    return (str + '')
+        .replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + breakTag + '$2');
+}
+
+function guid() {
+    function S4() {
+        return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+    }
+
+    return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
+}
+
+function number2string(number) {
+    string = '';
+    if (number >= 1000000000) {
+        floor1 = Math.floor(number / 10000000) / 100;
+        number = number - floor1 * 1000000000;
+        string = string + floor1 + 'b';
+    } else if (number >= 1000000) {
+        floor2 = Math.floor(number / 10000) / 100;
+        number = number - floor2 * 1000000;
+        string = string + floor2 + 'm';
+    } else if (number >= 1000) {
+        floor3 = Math.floor(number / 10) / 100;
+        number = number - floor3 * 1000;
+        string = string + floor3 + 'k';
+    } else if (number > 0) {
+        string = string + number;
+    } else {
+        string = '0';
+    }
+    return string;
+};
+
+function money_format(num) {
+    sign = (num == (num = Math.abs(num)));
+    num = Math.floor(num * 100 + 0.50000000001);
+    cents = num % 100;
+    num = Math.floor(num / 100).toString();
+    if (cents < 10) cents = "0" + cents;
+    for (var i = 0; i < Math.floor((num.length - (1 + i)) / 3); i++)
+        num = num.substring(0, num.length - (4 * i + 3)) + ',' + num.substring(num.length - (4 * i + 3));
+    money = (((sign) ? '' : '-') + num + '.' + cents);
+
+    return money;
+}
