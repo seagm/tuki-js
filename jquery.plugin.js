088a9abf60f23f3adef96c28b0f57dd08a4a93e3
大豆 <ranh@qq.com>
2017-08-14 17:01:27 +0800

    reformat

--- jquery.plugin.js
+++ jquery.plugin.js
@@ -1,10 +1,10 @@
-(function( $ ) {
-    $.popup = function(body, title) {
+(function ($) {
+    $.popup = function (body, title) {
         popup_box(body, title);
         return jQuery;
     };
- 
-    $.closePopup = function() {
+
+    $.closePopup = function () {
         close_popup_box();
         return jQuery;
     };
@@ -14,81 +14,81 @@
         return jQuery;
     },
 
-    $.notify = function (message) {
-        popup_notify(message);
-        return jQuery;
-    },
+        $.notify = function (message) {
+            popup_notify(message);
+            return jQuery;
+        },
 
-    $.redirect = function (href) {
-        location.href = href;
-        return jQuery;
-    },
+        $.redirect = function (href) {
+            location.href = href;
+            return jQuery;
+        },
 
-    $.refresh = function () {
-        location.reload();
-        return jQuery;
-    },
+        $.refresh = function () {
+            location.reload();
+            return jQuery;
+        },
 
-    $.reload =function () {
-        location.reload();
-        return jQuery;
-    },
+        $.reload = function () {
+            location.reload();
+            return jQuery;
+        },
 
-    $.postform = function (uri, postfields) {
-        $tip.removeClass('hide');
-        jQuery.post(uri, postfields, function (data) {
-            if (tipTimer == undefined) $tip.addClass('hide');
-            processResponse([], data);
-        }, 'json');
+        $.postform = function (uri, postfields) {
+            $tip.removeClass('hide');
+            jQuery.post(uri, postfields, function (data) {
+                if (tipTimer == undefined) $tip.addClass('hide');
+                processResponse([], data);
+            }, 'json');
 
-        return jQuery;
-    },
+            return jQuery;
+        },
 
-    $.iframe = function (url, w, h, c) {
-        popup_iframe(url, w, h, c);
+        $.iframe = function (url, w, h, c) {
+            popup_iframe(url, w, h, c);
 
-        return jQuery;
-    }
-}( jQuery ));
+            return jQuery;
+        }
+}(jQuery));
 
-(function( $ ) {
+(function ($) {
     $.fn.extend({
         //设置select的选项
-        setOptions: function(options, selected) {
+        setOptions: function (options, selected) {
             var $this = this;
             $this.empty();
             for (var key in options) {
                 if (key == selected)
-                    $this.append('<option value="'+key+'" selected>'+options[key]+'</option>');
+                    $this.append('<option value="' + key + '" selected>' + options[key] + '</option>');
                 else
-                    $this.append('<option value="'+key+'">'+options[key]+'</option>');
+                    $this.append('<option value="' + key + '">' + options[key] + '</option>');
             }
 
             return this;
         },
 
-        top: function() {
+        top: function () {
             var node = $(this);
-            while(node.parent().length) {
+            while (node.parent().length) {
                 node = node.parent();
             }
             return node;
         },
 
-        smsCon: function(Operation_code,message,dataurl) {
+        smsCon: function (Operation_code, message, dataurl) {
             var $this = this;
             //不同的code不同的操作
-            if(Operation_code == 1){
+            if (Operation_code == 1) {
                 $this.addClass('disable');
                 $this.prev('div.sms_note').removeClass('alert').addClass('success');
                 $this.prev('div.sms_note').text(message);
                 var text = $this.children().text();
                 var secends = 60;
-                var t = setInterval(function(){
+                var t = setInterval(function () {
                     if (--secends < 0) {
                         clearInterval(t);
                         $this.removeClass('disable').children().text('Send Code');
-                        $this.next().children('input[name=sms_verify_code]').attr('placeholder','Enter Verification Code').next().removeClass('incorrect');
+                        $this.next().children('input[name=sms_verify_code]').attr('placeholder', 'Enter Verification Code').next().removeClass('incorrect');
                     } else {
                         $this.children().text('Resend Code ' + ' ' + secends);
                     }
@@ -99,23 +99,23 @@
                 }, 1000);
 
                 return false;
-            }else if(Operation_code == -1){
+            } else if (Operation_code == -1) {
                 $this.prev('div.sms_note').removeClass('success').addClass('alert');
                 $this.prev('div.sms_note').text(message);
                 $this.next().next().addClass('hide').addClass('disable');
                 $this.next().children('input[name=sms_verify_code]').addClass('hide');
                 return false;
-            }else if(Operation_code == -2){
+            } else if (Operation_code == -2) {
                 $this.addClass('disable');
                 $this.prev('div.sms_note').removeClass('success').addClass('alert');
                 $this.prev('div.sms_note').text(message);
                 var text = $this.children().text();
                 var secends = 60;
-                var t = setInterval(function(){
+                var t = setInterval(function () {
                     if (--secends < 0) {
                         clearInterval(t);
                         $this.removeClass('disable').children().text('Send Code');
-                        $this.next().children('input[name=sms_verify_code]').attr('placeholder','Enter Verification Code').next().removeClass('incorrect');
+                        $this.next().children('input[name=sms_verify_code]').attr('placeholder', 'Enter Verification Code').next().removeClass('incorrect');
                     } else {
                         $this.children().text('Resend Code ' + ' ' + secends);
                     }
@@ -126,10 +126,10 @@
                 }, 1000);
 
                 return false;
-            }else if(Operation_code == -7){
+            } else if (Operation_code == -7) {
                 $this.addClass('disable');
                 $this.prev('div.sms_note').removeClass('success').addClass('alert');
-                var message =  '<a href=\"'+dataurl+'\">'+message+'</a>';
+                var message = '<a href=\"' + dataurl + '\">' + message + '</a>';
                 $this.prev('div.sms_note').html(message);
                 $this.next().next().addClass('hide').addClass('disable');
                 $this.next().children('input[name=sms_verify_code]').addClass('hide');
@@ -138,9 +138,9 @@
             return this;
         },
 
-        call_sms: function(Operation_code, message) {
+        call_sms: function (Operation_code, message) {
             var $this = this;
-            if(Operation_code == 1){
+            if (Operation_code == 1) {
                 $this.next().removeClass('disable').find('input').removeAttr('disabled');
                 $this.siblings(".btw").eq(0).prev('div.sms_note').removeClass('alert');
                 $this.siblings(".btw").eq(0).prev('div.sms_note').addClass('success');
@@ -148,12 +148,12 @@
                 $this.prev('div').remove();
                 $this.siblings(".btw").eq(0).remove();
                 $this.remove();
-            }else if(Operation_code == -3){
+            } else if (Operation_code == -3) {
                 $this.prev('div').children('input[name=sms_verify_code]').val('').next().removeClass('loading');
                 $this.siblings(".btw").eq(0).prev('div.sms_note').removeClass('success');
                 $this.siblings(".btw").eq(0).prev('div.sms_note').addClass('alert');
                 $this.siblings(".btw").eq(0).prev('div.sms_note').text(message);
-            }else{
+            } else {
                 $this.prev('div').children('input[name=sms_verify_code]').val('').next().removeClass('loading');
                 $this.siblings(".btw").eq(0).prev('div.sms_note').removeClass('success');
                 $this.siblings(".btw").eq(0).prev('div.sms_note').addClass('alert');
@@ -162,4 +162,4 @@
             return this;
         },
     });
-}( jQuery ));
+}(jQuery));
